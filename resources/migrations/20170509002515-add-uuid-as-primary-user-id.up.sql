d1be921d398b053c76ea1f5b25d033afe0bb5915
Mark Aiken <maiken@bitvore.com>
2017-05-09 00:39:24 -0700

    Add SQL migs to move id from int to random uuid

---
+++ resources/migrations/20170509002515-add-uuid-as-primary-user-id.up.sql
@@ -0,0 +1,31 @@
+ALTER TABLE users
+ADD COLUMN uid uuid NOT NULL
+CONSTRAINT gen_uuid_for_id DEFAULT gen_random_uuid();
+
+ALTER TABLE users 
+ADD UNIQUE(uid);
+
+ALTER TABLE categories
+ADD COLUMN user_uid uuid NOT NULL;
+
+ALTER TABLE tasks
+ADD COLUMN user_uid uuid NOT NULL;
+
+ALTER TABLE "categories" ADD CONSTRAINT "fk_categories_user_uid" FOREIGN KEY("user_uid")
+REFERENCES "users" ("uid");
+
+ALTER TABLE "tasks" ADD CONSTRAINT "fk_tasks_user_uid" FOREIGN KEY("user_uid")
+REFERENCES "users" ("uid");
+
+ALTER TABLE users DROP CONSTRAINT pk_users CASCADE;
+
+ALTER TABLE users
+DROP COLUMN id CASCADE;
+
+ALTER TABLE categories
+DROP COLUMN user_id;
+
+ALTER TABLE tasks
+DROP COLUMN user_id;
+
+ALTER TABLE users ADD PRIMARY KEY (uid);
