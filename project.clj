6059d240e2f6c3d2c25b526d335d5831d6b01c38
Mark Aiken <gwmaiken@att.net>
2017-10-08 02:57:25 -0700

    Sort dependencies by name

--- project.clj
+++ project.clj
@@ -3,13 +3,19 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
 
-  :dependencies [[buddy "1.3.0"]
+  :dependencies [[alandipert/storage-atom "2.0.1"]
+                 [buddy "1.3.0"]
                  [cljs-ajax "0.5.9"]
+                 [cljs-react-material-ui "0.2.43"]
+                 [cljsjs/js-joda "1.5.0-0"]
+                 [cljsjs/moment-timezone "0.5.11-0"]
+                 [clojure.java-time "0.3.0"]
+                 [com.cognitect/transit-cljs "0.8.239"]
                  [compojure "1.5.2"]
                  [conman "0.6.3"]
                  [cprop "0.1.10"]
-                 [day8.re-frame/http-fx "0.1.2"]
                  [day8.re-frame/async-flow-fx "0.0.6"]
+                 [day8.re-frame/http-fx "0.1.2"]
                  [funcool/struct "1.0.0"]
                  [luminus-immutant "0.2.3"]
                  [luminus-migrations "0.4.2"]
@@ -20,28 +26,22 @@
                  [metosin/ring-http-response "0.8.2"]
                  [mount "0.1.11"]
                  [org.clojure/clojure "1.9.0-alpha19"]
-                 [org.clojure/core.async "0.3.443"]
                  [org.clojure/clojurescript "1.9.521" :scope "provided"]
+                 [org.clojure/core.async "0.3.443"]
                  [org.clojure/tools.cli "0.3.5"]
                  [org.clojure/tools.logging "0.3.1"]
-                 [cljsjs/moment-timezone "0.5.11-0"]
-                 [cljsjs/js-joda "1.5.0-0"]
-                 [clojure.java-time "0.3.0"]
                  [org.postgresql/postgresql "42.0.0"]
                  [org.webjars.bower/tether "1.4.0"]
                  [org.webjars/font-awesome "4.7.0"]
                  [org.webjars/webjars-locator-jboss-vfs "0.1.0"]
                  [re-frame "0.9.2"]
-                 [cljs-react-material-ui "0.2.43"]
                  [reagent "0.6.1" :exclusions [cljsjs/react cljsjs/react-dom]]
                  [reagent-utils "0.2.1"]
+                 [reanimated "0.5.1"]
                  [ring-webjars "0.1.1"]
                  [ring/ring-core "1.6.0-RC3"]
                  [ring/ring-defaults "0.2.3"]
-                 [reanimated "0.5.1"]
                  [secretary "1.2.3"]
-                 [alandipert/storage-atom "2.0.1"]
-                 [com.cognitect/transit-cljs "0.8.239"]
                  [selmer "1.10.7"]]
 
 
@@ -60,7 +60,7 @@
             [lein-cljsbuild "1.1.5"]
             [lein-immutant "2.1.0"]]
   :clean-targets ^{:protect false}
-[:target-path [:cljsbuild :builds :app :compiler :output-dir] [:cljsbuild :builds :app :compiler :output-to]]
+  [:target-path [:cljsbuild :builds :app :compiler :output-dir] [:cljsbuild :builds :app :compiler :output-to]]
   :figwheel
   {:http-server-root "public"
    :nrepl-port       7002
@@ -69,66 +69,66 @@
 
 
   :profiles
-  {:uberjar       {:omit-source    true
-                   :prep-tasks     ["compile" ["cljsbuild" "once" "min"]]
-                   :cljsbuild
-                                   {:builds
-                                    {:min
-                                     {:source-paths ["src/cljc" "src/cljs" "env/prod/cljs"]
-                                      :compiler
-                                                    {:output-to     "target/cljsbuild/public/js/app.js"
-                                                     :optimizations :advanced
-                                                     :pretty-print  false
-                                                     :closure-warnings
-                                                                    {:externs-validation :off :non-standard-jsdoc :off}
-                                                     :externs       ["react/externs/react.js"]}}}}
+  {:uberjar     {:omit-source    true
+                 :prep-tasks     ["compile" ["cljsbuild" "once" "min"]]
+                 :cljsbuild
+                                 {:builds
+                                  {:min
+                                   {:source-paths ["src/cljc" "src/cljs" "env/prod/cljs"]
+                                    :compiler
+                                                  {:output-to     "target/cljsbuild/public/js/app.js"
+                                                   :optimizations :advanced
+                                                   :pretty-print  false
+                                                   :closure-warnings
+                                                                  {:externs-validation :off :non-standard-jsdoc :off}
+                                                   :externs       ["react/externs/react.js"]}}}}
 
 
-                   :aot            :all
-                   :uberjar-name   "time-align.jar"
-                   :source-paths   ["env/prod/clj"]
-                   :resource-paths ["env/prod/resources"]}
+                 :aot            :all
+                 :uberjar-name   "time-align.jar"
+                 :source-paths   ["env/prod/clj"]
+                 :resource-paths ["env/prod/resources"]}
 
-   :dev           [:project/dev :profiles/dev]
-   :test          [:project/dev :project/test :profiles/test]
+   :dev         [:project/dev :profiles/dev]
+   :test        [:project/dev :project/test :profiles/test]
 
-   :project/dev   {:dependencies   [[prone "1.1.4"]
-                                    [ring/ring-mock "0.3.0"]
-                                    [ring/ring-devel "1.5.1"]
-                                    [pjstadig/humane-test-output "0.8.1"]
-                                    [binaryage/devtools "0.9.3"]
-                                    [com.cemerick/piggieback "0.2.2-SNAPSHOT"]
-                                    [doo "0.1.7"]
-                                    [figwheel-sidecar "0.5.10"]
-                                    [org.clojure/test.check "0.9.0"]
-                                    [re-frisk "0.5.0"]]
+   :project/dev {:dependencies   [[binaryage/devtools "0.9.3"]
+                                  [com.cemerick/piggieback "0.2.2-SNAPSHOT"]
+                                  [doo "0.1.7"]
+                                  [figwheel-sidecar "0.5.10"]
+                                  [org.clojure/test.check "0.9.0"]
+                                  [pjstadig/humane-test-output "0.8.1"]
+                                  [prone "1.1.4"]
+                                  [re-frisk "0.5.0"]
+                                  [ring/ring-devel "1.5.1"]
+                                  [ring/ring-mock "0.3.0"]]
 
-                   :plugins        [[com.jakemccrary/lein-test-refresh "0.19.0"]
-                                    [lein-doo "0.1.7"]
-                                    [lein-figwheel "0.5.10"]
-                                    [org.clojure/clojurescript "1.9.521"]]
-                   :cljsbuild
-                                   {:builds
-                                    {:app
-                                     {:source-paths ["src/cljs" "src/cljc" "env/dev/cljs"]
-                                      :compiler
-                                                    {:main          "time-align.app"
-                                                     :asset-path    "/js/out"
-                                                     :output-to     "target/cljsbuild/public/js/app.js"
-                                                     :output-dir    "target/cljsbuild/public/js/out"
-                                                     :preloads      [re-frisk.preload]
-                                                     :source-map    true
-                                                     :optimizations :none
-                                                     :pretty-print  true}}}}
+                 :plugins        [[com.jakemccrary/lein-test-refresh "0.19.0"]
+                                  [lein-doo "0.1.7"]
+                                  [lein-figwheel "0.5.10"]
+                                  [org.clojure/clojurescript "1.9.521"]]
+                 :cljsbuild
+                                 {:builds
+                                  {:app
+                                   {:source-paths ["src/cljs" "src/cljc" "env/dev/cljs"]
+                                    :compiler
+                                                  {:main          "time-align.app"
+                                                   :asset-path    "/js/out"
+                                                   :output-to     "target/cljsbuild/public/js/app.js"
+                                                   :output-dir    "target/cljsbuild/public/js/out"
+                                                   :preloads      [re-frisk.preload]
+                                                   :source-map    true
+                                                   :optimizations :none
+                                                   :pretty-print  true}}}}
 
 
 
-                   :doo            {:build "test"}
-                   :source-paths   ["env/dev/clj"]
-                   :resource-paths ["env/dev/resources"]
-                   :repl-options   {:init-ns user}
-                   :injections     [(require 'pjstadig.humane-test-output)
-                                    (pjstadig.humane-test-output/activate!)]}
+                 :doo            {:build "test"}
+                 :source-paths   ["env/dev/clj"]
+                 :resource-paths ["env/dev/resources"]
+                 :repl-options   {:init-ns user}
+                 :injections     [(require 'pjstadig.humane-test-output)
+                                  (pjstadig.humane-test-output/activate!)]}
    :project/test  {:resource-paths ["env/test/resources"]
                    :cljsbuild
                                    {:builds
