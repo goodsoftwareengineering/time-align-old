65fd46306b443175abce0be754eb3a85081580ae
Mark Aiken <gwmaiken@att.net>
2017-10-13 01:39:44 -0700

    Add log back to rsynced folders, remove libsass from vagrantscript.sh

--- vagrantscript.sh
+++ vagrantscript.sh
@@ -13,17 +13,6 @@ sudo apt-get install -y openjdk-8-jre-headless
 
 sudo /var/lib/dpkg/info/ca-certificates-java.postinst configure
 
-cd /usr/local/lib
-sudo git clone https://github.com/sass/sassc.git --branch 3.3.6 --depth 1
-sudo git clone https://github.com/sass/libsass.git --branch 3.3.6 --depth 1
-cd libsass
-sudo autoreconf --force --install
-cd ..
-export SASS_LIBSASS_PATH=/usr/local/lib/libsass
-cd sassc
-sudo -E make
-ln /usr/local/lib/sassc/bin/sassc /usr/local/bin/sassc
-
 curl https://raw.githubusercontent.com/technomancy/leiningen/stable/bin/lein -o /usr/local/bin/lein
 chmod a+x /usr/local/bin/lein
 
