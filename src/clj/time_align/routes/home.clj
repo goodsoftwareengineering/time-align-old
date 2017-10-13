d62e77f728a56296a1266b5882cf0107306f24e4
Mark Aiken <gwmaiken@att.net>
2017-10-13 02:04:23 -0700

    Add analytics route and test route to get csrf token

--- src/clj/time_align/routes/home.clj
+++ src/clj/time_align/routes/home.clj
@@ -1,7 +1,10 @@
 (ns time-align.routes.home
   (:require [time-align.layout :as layout]
-            [compojure.core :refer [defroutes GET]]
+            [compojure.core :refer [defroutes GET POST]]
             [ring.util.http-response :as response]
+            [ring.middleware.anti-forgery :refer :all]
+            [time-align.db.core :as db]
+            [clojure.tools.logging :as log]
             [clojure.java.io :as io]))
 
 (defn home-page []
@@ -12,5 +15,16 @@
              (home-page))
            (GET "/docs" []
              (-> (response/ok (-> "docs/docs.md" io/resource slurp))
-                 (response/header "Content-Type" "text/plain; charset=utf-8"))))
-
+                 (response/header "Content-Type" "text/plain; charset=utf-8")))
+           (GET "/test-route" []
+             (fn [req]
+               (response/ok {:headers (:headers req)
+                             :csrf-token *anti-forgery-token*}))
+             )
+           (POST "/analytics" []
+             (fn [req]
+               (->> req
+                   :params
+                   db/create-analytic!
+                   (format "%d analytic(s) added")
+                   response/ok))))
