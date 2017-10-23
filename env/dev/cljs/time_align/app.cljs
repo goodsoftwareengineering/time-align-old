e7b53da5971b4510b97d0723409b3029aebc9451
Mark Aiken <gwmaiken@att.net>
2017-10-23 09:08:10 -0700

    Add support for figwheel repl over network
    Improve start-fw to autostart worker builder

--- env/dev/cljs/time_align/app.cljs
+++ env/dev/cljs/time_align/app.cljs
@@ -1,14 +1,9 @@
 (ns ^:figwheel-no-load time-align.app
   (:require [time-align.core :as core]
             [devtools.core :as devtools]
-            [figwheel.client :as figwheel :include-macros true]
             [re-frisk.core :refer [enable-re-frisk!]]))
 
 (enable-console-print!)
-
-(figwheel/watch-and-reload
-  :websocket-url "ws://localhost:3449/figwheel-ws"
-  :on-jsload core/mount-components)
 
 (devtools/install!)
 
