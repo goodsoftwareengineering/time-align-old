906f5420847ba5c272a07441699bc39b2be2be68
Mark Aiken <gwmaiken@att.net>
2017-11-13 07:10:16 -0800

    Fix figwheel startup conf, add auto-start to (cljs) call

--- env/dev/clj/time_align/figwheel.clj
+++ env/dev/clj/time_align/figwheel.clj
@@ -2,8 +2,8 @@
   (:require [figwheel-sidecar.repl-api :as ra]))
 
 (defn start-fw []
-  (ra/start-autobuild :worker)
-  (ra/start-figwheel!))
+  (ra/start-figwheel!)
+  (ra/start-autobuild :worker))
 
 (defn stop-fw []
   (ra/stop-figwheel!))
