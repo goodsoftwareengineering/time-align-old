77e8b9e78cc36dc152bc59098cfb48a259242103
jgoodhcg <jgoodhcg@gmail.com>
2018-01-12 14:18:44 -0500

    Refactor app-bar

--- src/cljs/time_align/ui/app_bar.cljs
+++ src/cljs/time_align/ui/app_bar.cljs
@@ -6,10 +6,70 @@
             [cljs-react-material-ui.icons :as ic]
             [time-align.js-interop :as jsi]))
 
+(def app-bar-options
+  [{:href "/#"
+    :label [:span "Home"]
+    :icon (uic/svg-mui-time-align {:color "black"
+                                   :style {:marginRight "0.5em"}})
+    :on-touch-tap #(do
+                     (rf/dispatch [:set-main-drawer false])
+                     (rf/dispatch
+                      [:set-active-page {:page-id :home}]))}
+
+   {:href "#/list"
+    :label [:span "List"]
+    :icon (uic/svg-mui-entity
+           {:type :all :color "black"
+            :style {:marginRight "0.5em"}})}
+
+   {:href "#/agenda"
+    :label [:span "Agenda"]
+    :icon [ic/action-view-agenda {:style {:marginRight "0.5em"}}]}
+
+   {:href "#/queue"
+    :label [:span "Queue"]
+    :icon [ic/action-toc {:style {:marginRight "0.5em"}}]}
+
+   {:label [:span "Settings"]
+    :icon [ic/action-settings {:style {:marginRight "0.5em"}}]}
+
+   {:label [:span "Account"]
+    :icon [ic/social-person {:style {:marginRight "0.5em"
+                                     :color       "grey"}}]}
+
+   {:label [:span "Import"]
+    :icon [ic/file-cloud-upload {:style {:marginRight "0.5em"}}]
+    :child [:input.import-file-input
+            {:type      "file"
+             :style     {:display "none"}
+             :on-change
+             (fn [e]
+               (let [inputter
+                     (-> (jsi/get-elements-by-class-name
+                          "import-file-input")
+                         (aget 0)
+                         (oget "files")
+                         (aget 0))]
+                 (rf/dispatch [:import-app-db inputter])))}]
+    :on-touch-tap (fn []
+                    (let [input
+                          (-> (jsi/get-elements-by-class-name
+                               "import-file-input")
+                              (aget 0))]
+                      (jsi/click! input)))}
+
+   {:on-touch-tap time-align.storage/export-app-db
+    :icon [ic/content-save {:style {:marginRight "0.5em"}}]
+    :label [:span "Export"]}])
+
+(defn app-bar-option [{:keys [href label icon on-touch-tap child]}]
+  [:a {:href href}
+   [ui/menu-item {:onTouchTap on-touch-tap
+                  :innerDivStyle {:display "flex" :align-items "center"}}
+    child icon label]])
+
 (defn app-bar []
-  (let [
-        main-drawer-state @(rf/subscribe [:main-drawer-state])
-        ]
+  (let [main-drawer-state @(rf/subscribe [:main-drawer-state])]
     [:div.app-bar-container
      {:style {:display    "flex"
               :flex       "1 0 100%"
@@ -22,63 +82,5 @@
                  :disableSwipeToOpen true
                  :onRequestChange    (fn [new-state] (rf/dispatch [:set-main-drawer new-state]))}
 
-      [:a {:href "/#"}
-       [ui/menu-item {:onTouchTap    #(do
-                                        (rf/dispatch [:set-main-drawer false])
-                                        (rf/dispatch [:set-active-page {:page-id :home}]))
-                      :innerDivStyle {:display "flex" :align-items "center"}}
-        (uic/svg-mui-time-align {:color "black"
-                             :style {:marginRight "0.5em"}})
-        [:span "Home"]]]
-
-      [:a {:href "#/list"}
-       [ui/menu-item {:innerDivStyle {:display "flex" :align-items "center"}}
-        (uic/svg-mui-entity {:type :all :color "black" :style {:marginRight "0.5em"}})
-        [:span "List"]]]
-
-      [:a {:href "#/agenda"}
-       [ui/menu-item {:innerDivStyle {:display "flex" :align-items "center"}}
-        [ic/action-view-agenda {:style {:marginRight "0.5em"}}]
-        [:span "Agenda"]]]
-
-      [:a {:href "#/queue"}
-       [ui/menu-item {:innerDivStyle {:display "flex" :align-items "center"}}
-        [ic/action-toc {:style {:marginRight "0.5em"}}]
-        [:span "Queue"]]]
-
-      [ui/menu-item {:innerDivStyle {:display "flex" :align-items "center"}
-                     :disabled      true}
-       [ic/action-settings {:style {:marginRight "0.5em"}}]
-       [:span "Settings"]]
-
-      [ui/menu-item {:innerDivStyle {:display "flex" :align-items "center"}
-                     :disabled      true}
-       [ic/social-person {:style {:marginRight "0.5em"
-                                  :color       "grey"}}]
-       [:span "Account"]]
-
-      [ui/menu-item {:innerDivStyle {:display "flex" :align-items "center"}
-                     :on-click      (fn []
-                                      (let [input (-> (jsi/get-elements-by-class-name "import-file-input")
-                                                      (aget 0))]
-                                        (jsi/click! input)))}
-       [:input.import-file-input {:type      "file"
-                                  :style     {:display "none"}
-                                  :on-change (fn [e]
-                                               (let [inputter (-> (jsi/get-elements-by-class-name "import-file-input")
-                                                                  (aget 0)
-                                                                  (oget "files")
-                                                                  (aget 0))]
-                                                 (rf/dispatch [:import-app-db inputter])))}]
-       [ic/file-cloud-upload {:style {:marginRight "0.5em"}}]
-       [:span "Import"]]
-
-      [ui/menu-item {:innerDivStyle {:display "flex" :align-items "center"}
-                     :on-click      time-align.storage/export-app-db}
-       [ic/content-save {:style {:marginRight "0.5em"}}]
-       [:span "Export"]]
-
-
-      ]]
-    )
-  )
+      (->> app-bar-options
+           (map app-bar-option))]]))
